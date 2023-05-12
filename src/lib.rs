950639ea696ae04a99ed65a871e61fef42e1ce6c
Sumner Hearth <sumnernh@gmail.com>
2023-05-12 01:57:38 -0400

    Add and mul

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,35 @@ use num_traits::{One, Zero};
 use pyo3::prelude::*;
 use pyo3::Python;
 use qip_iterators::iterators::MatrixOp;
-use qip_iterators::matrix_ops::{apply_op, apply_op_row};
+use qip_iterators::matrix_ops::{apply_op, apply_op_overwrite, apply_op_row};
 use rayon::prelude::*;
 use std::iter::Sum;
 use std::ops::{Add, AddAssign, Mul};
 
-/// Unlike the Lattice class this maintains a set of graphs with internal state.
 #[pyclass]
-pub struct Circuit {}
+pub struct TensorMatf64 {
+    mat: MatrixTree<f64>,
+}
 
 #[pymethods]
-impl Circuit {}
+impl TensorMatf64 {
+    #[new]
+    fn new(indices: Vec<usize>, data: Vec<f64>) -> Self {
+        Self {
+            mat: MatrixTree::Leaf(MatrixOp::new_matrix(indices, data)),
+        }
+    }
+
+    fn __add__(&self, other: &Self) -> Self {
+        let mat = self.mat.clone().add(other.mat.clone());
+        Self { mat }
+    }
+
+    fn __matmul__(&self, other: &Self) -> Self {
+        let mat = self.mat.clone().mul(other.mat.clone());
+        Self { mat }
+    }
+}
 
 #[derive(Clone)]
 pub enum MatrixTree<P> {
@@ -38,22 +56,21 @@ where
                         .sum()
                 });
             }
-            MatrixTree::Sum(ops) => ops.iter().for_each(|op| op.apply(n, input, output)),
-            MatrixTree::ProdLeaf(ops) => {
-                todo!()
-            }
-            MatrixTree::Prod(ops) => {
-                todo!()
+            MatrixTree::Sum(ops) => ops.iter().for_each(|op| op.apply_add(n, input, output)),
+            MatrixTree::ProdLeaf(_) | MatrixTree::Prod(_) => {
+                let out_copy = output.to_vec();
+                self.apply_overwrite(n, input, output);
+                output
+                    .iter_mut()
+                    .zip(out_copy.into_iter())
+                    .for_each(|(x, a)| *x += a);
             }
         }
     }
 
     pub fn apply_overwrite(&self, n: usize, input: &[P], output: &mut [P]) {
         match self {
-            MatrixTree::Leaf(op) => {
-                output.iter_mut().for_each(|x| *x = P::zero());
-                apply_op(n, op, input, output, 0, 0)
-            }
+            MatrixTree::Leaf(op) => apply_op_overwrite(n, op, input, output, 0, 0),
             MatrixTree::SumLeaf(ops) => {
                 output.par_iter_mut().enumerate().for_each(|(i, x)| {
                     *x = ops
@@ -64,14 +81,50 @@ where
             }
             MatrixTree::Sum(ops) => {
                 output.iter_mut().for_each(|x| *x = P::zero());
-                ops.iter().for_each(|op| op.apply(n, input, output))
+                ops.iter().for_each(|op| op.apply_add(n, input, output))
             }
-            MatrixTree::ProdLeaf(ops) => {
-                todo!()
-            }
-            MatrixTree::Prod(ops) => {
-                todo!()
-            }
+            MatrixTree::ProdLeaf(ops) => match ops.as_slice() {
+                [] => output
+                    .iter_mut()
+                    .zip(input.iter())
+                    .for_each(|(a, b)| *a = b.clone()),
+                [op] => {
+                    apply_op_overwrite(n, op, input, output, 0, 0);
+                }
+                [op, ops @ ..] => {
+                    let mut buffer = vec![P::zero(); output.len()];
+                    let (buffa, buffb) = if ops.len() % 2 == 0 {
+                        (output, buffer.as_mut_slice())
+                    } else {
+                        (buffer.as_mut_slice(), output)
+                    };
+                    apply_op_overwrite(n, op, input, buffa, 0, 0);
+                    ops.iter().fold((buffa, buffb), |(buffa, buffb), op| {
+                        apply_op_overwrite(n, op, buffa, buffb, 0, 0);
+                        (buffb, buffa)
+                    });
+                }
+            },
+            MatrixTree::Prod(ops) => match ops.as_slice() {
+                [] => output
+                    .iter_mut()
+                    .zip(input.iter())
+                    .for_each(|(a, b)| *a = b.clone()),
+                [op] => op.apply_overwrite(n, input, output),
+                [op, ops @ ..] => {
+                    let mut buffer = vec![P::zero(); output.len()];
+                    let (buffa, buffb) = if ops.len() % 2 == 0 {
+                        (output, buffer.as_mut_slice())
+                    } else {
+                        (buffer.as_mut_slice(), output)
+                    };
+                    op.apply_overwrite(n, input, buffa);
+                    ops.iter().fold((buffa, buffb), |(buffa, buffb), op| {
+                        op.apply_overwrite(n, buffa, buffb);
+                        (buffb, buffa)
+                    });
+                }
+            },
         }
     }
 }
@@ -140,7 +193,7 @@ impl<P> Mul for MatrixTree<P> {
 }
 
 #[pymodule]
-fn py_qip(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_class::<Circuit>()?;
+fn qubit_matmul(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<TensorMatf64>()?;
     Ok(())
 }
